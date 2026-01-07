373429ef23ba059f4c1e7c918f1071b06bfca279
VasilisTheof <theovasilis@gmail.com>
2026-01-07 12:59:22 +0100

    Update event formatting and improve spacing

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('events.json')
+fetch(`events.json?v=${new Date().getTime()}`)
   .then(res => res.json())
   .then(data => {
     const futureContainer = document.getElementById("future-events");
@@ -8,8 +8,11 @@ fetch('events.json')
     data.future.forEach(event => {
       const html = `
         <h3>${event.title}</h3>
-        <h4>${event.date}, ${event.time}, ${event.location}</h4>
-        <a href="${event.link}" target="_blank">${event.link}</a>
+        <p>
+          ${event.date}${event.time ? ', ' + event.time : ''}<br>
+          ${event.location ? event.location : ''}
+        </p>
+        ${event.link ? `<p><a href="${event.link}" target="_blank">${event.link}</a></p>` : ''}
       `;
       futureContainer.innerHTML += html;
     });
