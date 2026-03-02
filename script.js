86c25b071acbfa38bd2de3c6f67e7f0718474815
VasilisTheof <theovasilis@gmail.com>
2026-03-02 23:06:30 +0100

    Update 2026

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
 fetch(`events.json?v=${new Date().getTime()}`)
   .then(res => res.json())
   .then(data => {
-    const futureContainer = document.getElementById("future-events");
+
     const pastContainer = document.getElementById("past-events");
 
-    // Future events
-    data.future.forEach(event => {
-      const html = `
-        <h3>${event.title}</h3>
-        <p>
-          ${event.date}${event.time ? ', ' + event.time : ''}<br>
-          ${event.location ? event.location : ''}
-        </p>
-        ${event.link ? `<p><a href="${event.link}" target="_blank">${event.link}</a></p>` : ''}
-      `;
-      futureContainer.innerHTML += html;
-    });
-
-    // Past events
+    // Past events (loaded from JSON)
     data.past.forEach(event => {
       const html = `
         <h3>${event.title}</h3>
